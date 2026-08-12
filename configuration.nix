40ef0eaf0bb956f59f78d2cb052c7ad5c6bd585f
dedestem <david@davidnet.net>
2026-08-12 14:52:27 +0200

    Fix

--- configuration.nix
+++ configuration.nix
@@ -193,6 +193,23 @@
     }
   ];
 
+  services.flatpak.overrides = {
+    "com.modrinth.ModrinthApp" = {
+      Context = {
+        sockets = [
+          "x11"
+          "wayland"
+        ];
+        devices = [ "dri" ];
+      };
+      Environment = {
+        DISPLAY = ":0";
+        __NV_PRIME_RENDER_OFFLOAD = "1";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      };
+    };
+  };
+
   # Other programs
   services.printing.enable = true;
   programs.zsh.enable = true;
